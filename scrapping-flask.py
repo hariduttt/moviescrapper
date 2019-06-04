4c24522d5e5c5e63308abee42d9df464e600d146
Haridutt <hariduttjani@gmail.com>
2019-06-04 15:05:20 +0530

    Bugless version.

--- scrapping-flask.py
+++ scrapping-flask.py
@@ -8,40 +8,31 @@ from scrapping import Scrapper
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/movie-data/<web_link>/<tag>/<html_class>/<iterations>')
-def movie_data(web_link, tag, html_class, iterations):
+@app.route('/movie-data/<tag>/<html_class>/<int:iterations>')
+def movie_data(tag, html_class, iterations):
     database_file = "imdbsqlfile.db"
     table_name = "imdbdata"
     csv_file_name = "imdbmovies.csv"
+    web_url = "https://www.imdb.com/search/title?"\
+                "genres=drama&groups=top_250&sort=user_rating,desc"
+    
     imdb_scrapper = Scrapper(table_name, database_file, csv_file_name)
     
     col_1, col_2, col_3, col_4 = imdb_scrapper.default_column()
 
-    scrapped_data = imdb_scrapper.scrap(website_link, tag,
+    scrapped_data = imdb_scrapper.scrap(web_url, tag,
                                         html_class, iterations)
     
     movie_rank, movie_name,\
     movie_year = imdb_scrapper.get_text_from_scrapped_data(scrapped_data)
     
-    movie_rating = imdb_scrapper.scrap(website_link, "div",
+    movie_rating = imdb_scrapper.scrap(web_url, "div",
                                  "ratings-bar strong", iterations)
+    sized = len(movie_rank)
     
-    movie_collumns_dictionary = {col_1:movie_rank, col_2:movie_name,
-                                 col_3:movie_year, col_4:movie_rating}
-    
-   return render_template("movie-data.html",
-                          data = movie_collumns_dictionary)
-
-'''@app.route('/guest/<guest>')
-def hello_guest(guest):
-   return 'Hello %s as Guest' % guest
-
-@app.route('/user/<name>')
-def hello_user(name):
-   if name =='admin':
-      return redirect(url_for('hello_admin'))
-   else:
-      return redirect(url_for('hello_guest',guest = name))'''
+    return render_template("movie-data.html", data1 = movie_rank,
+                           data2 = movie_name, data3 = movie_year,
+                           data4 = movie_rating, size = sized)
 
 if __name__ == '__main__':
    app.run()

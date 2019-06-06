459da55c1b2060e46466954dcb7ac8a05752a08f
Haridutt <hariduttjani@gmail.com>
2019-06-06 12:41:33 +0530

    Feature added : User can sort based on different attributes.

--- scrapping-flask.py
+++ scrapping-flask.py
@@ -5,10 +5,12 @@ Created on Tue Jun  4 11:47:37 2019
 """
 from scrapping import Scrapper
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/movie-data/<int:start>/<tag>/<html_class>/<int:iterations>')
-def movie_data_base(start, tag, html_class, iterations):
+@app.route('/movie-data/<int:start>/<tag>/<html_class>/<int:iterations>/'\
+           '<int:sorter>')
+def movie_data_base(start, tag, html_class, iterations, sorter):    
     if(start == 1):
         web_url = "https://www.imdb.com/search/title?"\
                     "genres=drama&groups=top_250&sort=user_rating,desc"
@@ -29,14 +31,24 @@ def movie_data_base(start, tag, html_class, iterations):
     
     movie_rating = imdb_scrapper.scrap(web_url, "div",
                                        "ratings-bar strong", iterations)
-    
     movie_links = imdb_scrapper.get_link(web_url, tag,
                                          html_class, iterations)
     sized = len(movie_rank)
+    for x in range(sized):
+        movie_rank[x] = int(movie_rank[x].split('.')[0])
+    l = list(zip(movie_rank, movie_name, movie_year,
+                 movie_rating, movie_links))
+    if(sorter == 0):
+        l.sort(key = lambda t:t[0])
+    elif(sorter == 1):
+        l.sort(key = lambda t:t[1])
+    elif(sorter == 2):
+        l.sort(key = lambda t:t[2])
+    elif(sorter == 3):
+        l.sort(key = lambda t:t[3])
+        
+    return render_template("movie-data.html", data = l, size = sized)
+
     
-    return render_template("movie-data.html", data1 = movie_rank,
-                           data2 = movie_name, data3 = movie_year,
-                           data4 = movie_rating, size = sized,
-                           links = movie_links)
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug=True)
